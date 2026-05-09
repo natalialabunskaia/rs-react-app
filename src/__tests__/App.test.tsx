d8fd756900686d39193283cf29a3f2aa5a3a0518
Natalia Labunskaia <nlabunskya@gmail.com>
2026-05-09 15:17:16 +0400

    test: add search component tests

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,13 @@
+import { it } from 'vitest'
+
+// Displays previously saved search term from localStorage on mount - перенос в App
+// Shows empty input when no saved term exists- перенос в App
+// Saves search term to localStorage when search button is clicked  - перенос в App
+// LocalStorage Integration:  - перенос в App
+// Retrieves saved search term on component mount  - перенос в App
+// Overwrites existing localStorage value when new search is performed  - перенос в App
+// Trims whitespace from search input before saving- перенос в App
+// 'Updates input value when user types' - перенос в App
+
+it('render search component', () => {})
+
--- src/__tests__/components/Search.test.tsx
+++ src/__tests__/components/Search.test.tsx
@@ -1,8 +1,13 @@
-import { it, describe, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { it, describe, expect, vi, afterEach } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Search from '../../app/components/Search';
 
-describe('Search', () => {
+afterEach(() => {
+  cleanup();
+});
+
+describe('Rendering Tests', () => {
   it('should render Search input field', () => {
     render(
       <Search searchTerm="" onChange={vi.fn()} onSubmit={vi.fn()} error="" />
@@ -10,5 +15,73 @@ describe('Search', () => {
 
     const input = screen.getByLabelText('pokemon-type');
     expect(input).toBeInTheDocument();
+  });
+
+  it('should render Search button', () => {
+    render(
+      <Search searchTerm="" onChange={vi.fn()} onSubmit={vi.fn()} error="" />
+    );
+    const button = screen.getByLabelText('search');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should render Example paragraph', () => {
+    render(
+      <Search searchTerm="" onChange={vi.fn()} onSubmit={vi.fn()} error="" />
+    );
+    const paragraph = screen.getByText(/Example:/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it('should render input with search term value', () => {
+    const searchTerm = 'fire';
+    render(
+      <Search
+        searchTerm={searchTerm}
+        onChange={vi.fn()}
+        onSubmit={vi.fn()}
+        error=""
+      />
+    );
+
+    const input = screen.getByLabelText('pokemon-type');
+    expect(input).toHaveValue(searchTerm);
+  });
+});
+
+describe('User Interaction tests', () => {
+  it('should call onChange when user types in input', async () => {
+    const user = userEvent.setup();
+    const handleChange = vi.fn();
+
+    render(
+      <Search
+        searchTerm=""
+        onChange={handleChange}
+        onSubmit={vi.fn()}
+        error=""
+      />
+    );
+
+    const input = screen.getByLabelText('pokemon-type');
+    await user.type(input, 'fire');
+    expect(handleChange).toHaveBeenCalled();
+  });
+  it('should call onSubmit when user clicks on Search button', async () => {
+    const user = userEvent.setup();
+    const handleSubmit = vi.fn((event) => event.preventDefault());
+
+    render(
+      <Search
+        searchTerm=""
+        onChange={vi.fn()}
+        onSubmit={handleSubmit}
+        error=""
+      />
+    );
+
+    const button = screen.getByLabelText('search');
+    await user.click(button);
+    expect(handleSubmit).toHaveBeenCalled();    
   });
 });
